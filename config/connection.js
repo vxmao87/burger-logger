a4f350c0b95dc8a7697e432ac11791ac538bcfed
Vinh Mao <vinhxmao87@gmail.com>
2020-06-01 14:53:13 -0700

    connection and ORM files created and populated

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Set up MYSQL connection
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: burgers_db
+});
+
+// Start the connection
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connection: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection for use in the ORM
+module.exports = connection;

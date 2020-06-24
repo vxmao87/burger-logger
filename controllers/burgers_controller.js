45871bdf8724e64ceb750dfee636cdf35cbbda89
Vinh Mao <vinhxmao87@gmail.com>
2020-06-24 14:14:57 -0700

    burger controller routes updated with comments and readability

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,30 +2,30 @@ var express = require("express");
 var burger = require("../models/burger.js")
 var router = express.Router();
 
+// GET route to select all burgers
 router.get("/", function(req, res) {
     burger.selectAll(function (data) {
         var hbsObject = {
             burger: data,
         };
-        console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 
+// POST route for putting in a new burger
 router.post("/api/burgers", function (req, res) {
     burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
         res.json({ id: result.insertId });
     });
 });
 
+// UPDATE route for changing the status of a burger
 router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
     burger.updateOne(
         {
             devoured: req.body.devoured,
         },
-        condition,
+        req.params.id,
         function (result) {
             if(result.changedRows === 0) {
                 return res.status(404).end();

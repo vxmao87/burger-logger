0206250f07af21e61512a421620b2051a06b50a0
Vinh Mao <vinhxmao87@gmail.com>
2020-06-24 21:16:17 -0700

    Heroku connection code added

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,17 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: "burgers_db"
-});
-
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: "burgers_db"
+    });
+};
 // Start the connection
 connection.connect(function (err) {
     if (err) {
